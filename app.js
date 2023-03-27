3de0e365a6bed652455aec05dba43b9c3cdb5c4a
sihar <luq5nvd@gmail.com>
2023-03-27 21:24:58 +0700

    post request transaction

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const _=require('lodash')
-const responseTransactionList = require('./response')
+
+const { toArray } = require('lodash')
 const app = express()
 const db = require(`${__dirname}/connection`)
-const response = require(`${__dirname}/response`)
+
+// const responseTransactionList = require('./response')
+const response = require('./response')
+
 
 app.use(
-    bodyParser.urlencoded({extended:true})
+    bodyParser.json()
 )
+
 
 // login page
 app.get("/login", (req, res) => {
@@ -147,52 +152,142 @@ app.route("/item/:item_name")
 app.route("/transaction")
     .get((req, res) => {
         db.query(`
-        SELECT transaksi.id_transaksi AS idTransaction,
-        user_app.user_name AS createBy, 
-        transaksi.created_date AS createdDate,
-        transaksi.nomor_transaksi AS numberTransaction, 
-        transaksi.nomor_so AS numberSO,
-        status.status_name AS statusTransaction,
-        transaksi.total_pembelian AS totalAmount
+        SELECT transaksi.transaksi_id,
+        user_app.user_name, 
+        transaksi.created_date,
+        transaksi.nomor_so,
+        status.status_name,
+        transaksi.total_pembelian
         FROM transaksi
-        INNER JOIN user_app ON transaksi.fk_id_user = user_app.id_user
-        INNER JOIN status ON transaksi.fk_id_status = status.id_status`, (err,transactionList,ield) => {
+        INNER JOIN user_app ON transaksi.fk_id_user = user_app.user_id
+        INNER JOIN status ON transaksi.fk_id_status = status.status_id`, (err,transactionList,ield) => {
             if (err) throw err
             else {
-                db.query(`SELECT COUNT(id_transaksi) AS totalTransaction
+                // res.send(transactionList)
+                db.query(`SELECT COUNT(transaksi_id) AS totalTransaction
                 FROM transaksi`, (err, totalData, ield) => {
+                    if (err) throw `${err}`
+                    else {
+                        responseTransactionList(
+                            200,
+                            transactionList,
+                            totalData,
+                            `success get ${parseInt(JSON.stringify(totalData[0].totalTransaction))} transaction`,
+                            res
+                        )
+                    }
+                })
+            }
+        })
+    })
+
+    .post((req, res) => {
+        const totalAmount = req.body.totalAmount
+        const userId = req.body.userId
+        const numberSo = req.body.numberSo
+        const transactionDescription = req.body.transactionDescription
+
+        const idItem = req.body.idItem
+        const itemDeskripsi = req.body.itemDeskripsi
+        const itemQty = req.body.itemQty
+        const itemPrice = req.body.itemPrice
+
+        const currentDate = new Date()
+        const dd =currentDate.getDate()
+        const mm =currentDate.getMonth()+1
+        const yyyy =currentDate.getFullYear()
+
+        const createdDate = `${yyyy}-${mm}-${dd}`
+
+        db.query(`INSERT INTO transaksi
+        (transaksi_id, total_pembelian, fk_id_user, nomor_so, fk_id_status, transaksi_deskripsi, created_date,updated_date)
+        VALUES
+        (DEFAULT, ${totalAmount}, ${userId}, '${numberSo}',1, '${transactionDescription}', '${createdDate}','${createdDate}')`, (err, transactionResult) => {
+            if (err) throw err
+            else {
+                db.query(`SELECT * FROM transaksi 
+                ORDER BY transaksi_id DESC LIMIT 1`, (err, transactionId) => {
                     if (err) throw err
-                    else
-                    responseTransactionList(
-                        200,
-                        transactionList,
-                        totalData,
-                        `success`,
-                        res
-                    )
+                    else {
+                        const newTransactionId = transactionId[0].transaksi_id
+                        db.query(`INSERT INTO item_transaksi 
+                        (item_transaksi_id, fk_id_item, fk_id_transaksi, deskripsi_item_transaksi, qty, harga_satuan) 
+                        VALUES (DEFAULT,${idItem},${newTransactionId},'${itemDeskripsi}',${itemQty},${itemPrice})`, (err, itemTransactionResult) => {
+                        if (err) throw err
+                        else response.responeUpdate(
+                                200,
+                                itemTransactionResult,
+                                `transaksi berhasil dibuat`,
+                                res
+                            )
+                        })
+                    }
                 })
             }
         })
     })
 
 
-app.route("/transaction/:idTransaction")
+app.route("/transaction/detail")
+    .get((req, res) => {
+        db.query(`SELECT * FROM item_transaksi`, (err, result) => {
+            if (err) throw err
+            else
+                response.responseTransactionList(
+                200,
+                result,
+                _,
+                `successget all detail transaction`,
+                res
+            )
+        })
+    })
+
+    .post((req, res) => {
+
+        const idItem = req.body.idItem
+        const idTransaksi = req.body.idTransaksi
+        const itemDeskripsi = req.body.itemDeskripsi
+        const itemQty = req.body.itemQty
+        const itemPrice = req.body.itemPrice
+        db.query(`INSERT INTO item_transaksi 
+        (item_transaksi_id, fk_id_item, fk_id_transaksi, deskripsi_item_transaksi, qty, harga_satuan) 
+        VALUES (DEFAULT,${idItem},${idTransaksi},'${itemDeskripsi}',${itemQty},${itemPrice})`, (err, result) => {
+            if (err) throw err
+            else response.responeUpdate(
+                200,
+                result,
+                `item transaksi berhasil dibuat`,
+                res
+            )
+        })
+    })
+
+
+
+app.route("/transaction/detail/:idTransaction")
     .get((req, res) => {
         const idTransaction=req.params.idTransaction
         db.query(`SELECT item.item_name,
-        item_transaksi.deskripsi_item,
+        item_transaksi.deskripsi_item_transaksi,
         item_transaksi.qty,
         item_transaksi.harga_satuan
         FROM item_transaksi
-        INNER JOIN item ON item_transaksi.fk_id_item = item.id_item
+        INNER JOIN item ON item_transaksi.fk_id_item = item.item_id
         WHERE fk_id_transaksi =${idTransaction}`, (err, row, ield) => {
             if (err) throw err
-            else res.send(row)
+            else response.responeUpdate(
+                200,
+                row,
+                `detail transactionID ${idTransaction} berhasil ditampilkan`,
+                res
+            )
         })
     })
 
     .put((req, res) => {
         const idTransaction = req.params.idTransaction
+        
         const updateTotalAmount = req.body.updateTotalAmount
         const updateCreatedBy = req.body.updateCreatedBy
         const updateSalesNumber =req.body.updateSalesNumber
@@ -210,13 +305,15 @@ app.route("/transaction/:idTransaction")
         transaksi.nomor_so='${updateSalesNumber}',
         transaksi.fk_id_status=${updateStatus},
         transaksi.updated_date='${updatedDate}'
-        WHERE transaksi.id_transaksi =${idTransaction}`, (err, result) => {
-            if (err) throw err
-            else response(
-                200,
-                `transaksi nomor ${idTransaction} berhasil diubah`,
-                res
-            )
+        WHERE transaksi.transaksi_id = ${idTransaction}`, (err, result) => {
+            if (err) res.send(`put/transaction/:id = ${err}`) 
+            else 
+                response.responeUpdate(
+                    200,
+                    result,
+                    `data berhasil diubah`,
+                    res
+                )
         })
     })
 
